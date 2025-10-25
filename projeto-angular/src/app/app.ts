662b8a07c9576849592aa7f39ef5f0882786f374
Aguinaldo Neto <aguinaldo.netodev@gmail.com>
2025-10-25 20:20:06 -0300

    create project

---
+++ projeto-angular/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { Page } from "./components/shared/page/page";
+
+@Component({
+  selector: 'app-root',
+  imports: [Page],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+}
